2e692572cd9810806d381bcf50f49824cef587b1
catsinspacesuits <simunro@hotmail.co.uk>
2019-05-17 16:47:39 +0200

    add clear button

--- Gemfile
+++ Gemfile
@@ -16,7 +16,6 @@ gem 'sassc-rails', '>= 2.1.0'
 gem 'simple_form', '~> 4.0', '>= 4.0.1'
 gem 'sqlite3'
 gem 'sprockets-rails', :require => 'sprockets/railtie'
-gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,31 +1,19 @@
 // FIND COORDINATES OF CURRENT LOCATION  
 
-var currentLocation = document.getElementById('coordinatesStore'); 
-document.querySelector('.add-button').addEventListener('click', () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude }}) => {
-    currentLocation.value = latitude + ", " + longitude;
+window.onload = function() {
+    var currentLocation = document.getElementById('coordinatesStore');
+    document.querySelector('.add-button').addEventListener('click', () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(({
+                coords: {
+                    latitude,
+                    longitude
+                }
+            }) => {
+                currentLocation.value = latitude + ", " + longitude;
+            });
+        } else {
+            currentLocation.value = "Geolocation is not supported by this browser.";
+        }
     });
-  } else {
-    currentLocation.value = "Geolocation is not supported by this browser.";
-  }
-});
-
-    // CLEAR FORM FIELDS
-
-// var clearField = document.querySelectorAll('.clear-button');
-// document.querySelectorAll('.clear-button').addEventListener('click', () => clearField());
-//     function clearField() {
-//     currentLocation.value = "";
-//       }
-
-    // Array.prototype.forEach.call(
-    //   document.querySelectorAll('.clear-button'),
-    //   (button, i) => {
-    //     button.addEventListener('click', () => clearField(i));
-    //   }
-    // );
-
-    // function clearField() {
-    //     currentLocation.value = "";
-    // }
+}
